a08684993d53a3c530456014b4b63555949ec0ae
Jerry Ma <jerryma@nospam>
2013-04-15 15:56:41 -0400

    Flesh out share models

--- README.rst
+++ README.rst
@@ -2,3 +2,9 @@ PictureShare
 ============
 
 Django picture sharing site
+
+Development
+===========
+
+This site is currently being developed on a Micro AWS EC2 Instance (Ubuntu).
+The development server is linked by default to ec.ohofzero.com:8080.
--- urls.py
+++ urls.py
@@ -15,3 +15,10 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
+
+from django.conf import settings
+
+if settings.DEBUG:
+        urlpatterns += patterns('',
+                       (r'^storage/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+                       )
